397546f47927c21db59a6ee1d0ebfd4cb4a87d7a
github username <github email address>
2019-12-18 19:19:40 -0500

    Done.

--- draft.rb
+++ draft.rb
@@ -24,7 +24,7 @@ def find_item_by_name_in_collection(name, collection)
   
   
   # # rspec spec/grocer_spec.rb:49 
-  
+  # VERSION 1
   def consolidate_cart(cart)
     concart = []
     element_index = 0 
@@ -56,6 +56,8 @@ def find_item_by_name_in_collection(name, collection)
   
   #        no implicit conversion of Symbol into Integer
   
+
+
   
   
   # PSEUDO 
@@ -83,8 +85,176 @@ def find_item_by_name_in_collection(name, collection)
   
   
   
+# VERSION 2 
+
+def consolidate_cart(cart)
+
+concart = []
+element_index = 0
+
+while element_index < cart.length do
+grocery_hash = cart[element_index] # √ (90% certain) # grocery_hash[:item] => returns an item name, eg Tempeh Avocado Cheese
+item_in_concart = find_item_by_name_in_collection(grocery_hash[:item], concart) # item_in_concart seems to be working, issue is what to do with return value
+  # returns item HASH if present or nil if absent
+  
+  if item_in_concart
+    concart.push(grocery_hash)
+    concart[grocery_hash][:count] += 1 # implemented below change for consistency
+  else concart.push(grocery_hash)
+    concart[grocery_hash][:count] = 1 # [item_in_cart] ∆ [grocery_hash] bc former should be nil in this case
+      # line 29 is wrong. 
+  end 
+    element_index += 1 
+end
+  puts concart
+  puts 1234567
+  concart
+end 
   
   
   
+# V3
+# rspec spec/grocer_spec.rb:77
+def apply_coupons(cart, coupons)
+  coupon_cart = []
+  pos = 0 # position
+
+  while pos < cart.length do
+    cart_hash = cart[pos]
+    matching_coupon_hash = find_item_by_name_in_collection(cart[pos][:item], coupons)
+
+    if coupons.length == 0 
+      return cart 
+    end
+    
+    # if !matching_coupon_hash
+    #   coupon_cart.push(cart[pos][:item]) 
+    # don't need this because changing to use old cart, getting rid of coupon cart
+    # end
+
+    if matching_coupon_hash && cart_hash[:count] >= matching_coupon_hash[:num]
+      new_hash = cart_hash # let's say this is the one that has coupons applied
+      new_hash[:item] = new_hash[:item] + ' W/COUPON'
+      new_hash[:price] = (matching_coupon_hash[:cost]/cart_hash[:count]).floor # seems to be rounding down effectively
+
+      
+      new_hash[:count] = (cart_hash[:count]/matching_coupon_hash[:num]).floor # this one is broken and needs fixing 
+      # why is key item instead of count? cart_hash[:item]
+      # Failing this test:       removes the number of discounted items from the original item's count (FAILED - 2)
+      # fixing this will solve the latter half of this method
+      
+      # adjustments go here:  
+      
+      # Then we adjust that item hash's count to be the result of
+      # item :count % coupon :num
+      # Then we use that item hash's features to push a new hash to that array
+      # Then we adjust that new hash's features
+      # we concatenate 'W/COUPON' onto the :item key
+      # we make the :count equal item :count / coupon :num rounded down
+      # we make the :price equal coupons :cost / items :count
+      coupon_cart.push(new_hash)
+    end 
+    pos += 1 
+  end
+
+
+  # V4
+
+  def apply_coupons(cart, coupons)
+    coupon_cart = []
+    pos = 0 # position
+  
+    while pos < cart.length do
+      cart_hash = cart[pos]
+      matching_coupon_hash = find_item_by_name_in_collection(cart[pos][:item], coupons)
+  
+      if coupons.length == 0 
+        return cart 
+      end
+      
+      # if !matching_coupon_hash
+      #   coupon_cart.push(cart[pos][:item]) 
+      # don't need this because changing to use old cart, getting rid of coupon cart
+      # end
+  
+      if matching_coupon_hash && cart_hash[:count] >= matching_coupon_hash[:num] 
+        #if matching coupon exists AND the we have sufficient number of items in cart to apply coupon
+        if cart_hash[:count] == matching_coupon_hash[:num]
+          cart_hash[:item] = cart_hash[:item] + ' W/COUPON'
+          cart_hash[:price] = (matching_coupon_hash[:cost]/cart_hash[:count]).round(2) # UPDATED, SEE RESULTS!
+          # cart_hash[:count] = (cart_hash[:count]/matching_coupon_hash[:num]).floor # this line should be unneccessary
+          ## matching_coupon_hash.delete how do I delete that hash? 
+        end 
+  
+        ## cart_hash[:item] = new_hash[:item] + ' W/COUPON'
+        ## new_hash[:price] = (matching_coupon_hash[:cost]/cart_hash[:count]).floor # seems to be rounding down effectively
   
   
+        ### new_hash[:count] = (cart_hash[:count]/matching_coupon_hash[:num]).floor # this one is broken and needs fixing 
+        # why is key item instead of count? cart_hash[:item]
+        # Failing this test:       removes the number of discounted items from the original item's count (FAILED - 2)
+        # fixing this will solve the latter half of this method
+        
+        # adjustments go here:  
+        
+        # Then we adjust that item hash's count to be the result of
+        # item :count % coupon :num
+        # Then we use that item hash's features to push a new hash to that array
+        # Then we adjust that new hash's features
+        # we concatenate 'W/COUPON' onto the :item key
+        # we make the :count equal item :count / coupon :num rounded down
+        # we make the :price equal coupons :cost / items :count
+        ## coupon_cart.push(new_hash)
+      end 
+      pos += 1 
+    end
+  
+    puts 'coupon_cart'
+    pp coupon_cart # => [{:item=>"CHEESE", :price=>6.5, :clearance=>false, :count=>4},{:item=>"AVOCADO", :price=>3.0, :clearance=>true, :count=>3}]
+    
+    puts 'coupons'
+    pp coupons # => [{:item=>"AVOCADO", :num=>2, :cost=>5.0},{:item=>"CHEESE", :num=>3, :cost=>15.0}]
+  
+    puts 'cart'
+    pp cart
+    
+    coupon_cart
+  end 
+    
+    # POINTERS # pp cart[pos][:count] # pp coupons[0][:cost] √ 
+  
+    
+    # we receive the cart and coupons, which are both AoH format
+    # this is a bit complicated, so let's create coupon_cart # actually no for now
+    # first we want to check each item in the cart
+    # then we want to compare that item to every coupon and see if they match
+    # NEW: if no cart + coupon match, just add item to cart √ 
+  
+    # if the item's [:item] key matches the coupon's [:item] key
+    # && the item's [:count] key >= the coupon's [:num]
+    
+    # THEN we add the item hash to coupon_cart XXXX
+    # PREVIOUS STEP IS WRONG AND THUS SKIPPED FOR NOW. ONCE WE PUSH TO COUPON_CART WE SHOULD BE DONE. ADJUSTMENTS MUST BE MADE BEFORE PUSHING. 
+  
+    # Then we adjust that item hash's count to be the result of
+    # item :count % coupon :num
+    # Then we use that item hash's features to push a new hash to that array
+    # Then we adjust that new hash's features
+    # we concatenate 'W/COUPON' onto the :item key
+    # we make the :count equal item :count / coupon :num rounded down
+    # we make the :price equal coupons :cost / items :count
+    
+    # if the item doesn't match any coupon we simply add it to the coupon_cart 
+    # and move on
+  
+  
+  
+    # CODE: if cart[i][:count] % coupons[:num]
+    #         new_cart.push({cart[i][:item]+ 'W/COUPON', cart[i][:price].... UNFINISHED, jumped back to pseudo})
+    #         cart[i][:count] = cart[i][:count] % coupons[:num]
+    #         changing the
+    #       end 
+    # and set 
+  
+    # Consult README for inputs and outputs
+    # REMEMBER: This method **should** update cart
--- grocer.rb
+++ grocer.rb
@@ -1,57 +1,86 @@
-require "pry"
-
 def find_item_by_name_in_collection(name, collection)
-  item_index = 0
-    
-  while item_index < collection.length do 
-    if collection[item_index][:item] == name 
-      return collection[item_index]
+  counter = 0
+  while counter < collection.length do 
+    if collection[counter][:item] == name 
+      return collection[counter]
     end 
-    item_index += 1 
+    counter += 1 
   end
-  nil 
 end
 
+# rspec spec/grocer_spec.rb:49
 def consolidate_cart(cart)
-  pp cart 
-  cart 
- 
-  concart = []
-  element_index = 0 
- 
-  
-  #grocery_hash = cart[element_index]
-  #item_in_concart = find_item_by_name_in_collection(grocery_hash[:item], concart)
- 
-  #puts item_in_concart
- 
-  puts 10101010101
-  puts concart 
-  puts 10101010101
-  concart
+  new_cart = [] 
+  counter = 0
+  while counter < cart.length
+    if find_item_by_name_in_collection(cart[counter][:item], new_cart)  #item hash, item IS present
+      find_item_by_name_in_collection(cart[counter][:item], new_cart)[:count] += 1
+    end 
+    if !find_item_by_name_in_collection(cart[counter][:item], new_cart) #nil, no item present
+      cart[counter][:count] = 1
+      new_cart.push(cart[counter])
+    end
+    counter += 1 
+  end
+  new_cart
 end
 
-
+# ends at 50:00, summary ends at 52:35 https://www.youtube.com/watch?v=JAOL4dbtfLQ&t=28s
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0
+  while counter < coupons.length
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+    # designed to deal with multiple coupons. Will help us increment item count if 
+    # if couponed item is already in cart
+    if cart_item && cart_item[:count] >= coupons[counter][:num] # returns cart item or nil
+      if cart_item_with_coupon 
+        cart_item_with_coupon[:count] += coupons[counter][:num]
+        cart_item[:count] -= coupons[counter][:num]
+      else 
+        cart_item_with_coupon = {
+          item: couponed_item_name,
+          price: coupons[counter][:cost] / coupons[counter][:num],
+          count: coupons[counter][:num],
+          clearance: cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[counter][:num]
+      end 
+    end 
+    counter += 1
+  end 
+  cart 
+end 
+ 
+# rspec spec/grocer_spec.rb:196
+def apply_clearance(cart)
+  counter = 0 
+  while counter < cart.length do
+    if cart[counter][:clearance] == true 
+      cart[counter][:price] = (cart[counter][:price] * 0.8).round(2)
+    end 
+    counter += 1 
+  end 
+  cart
 end
 
-def apply_clearance(cart)
-  # Consult README for inputs and outputs
+# rspec spec/grocer_spec.rb:225
   #
-  # REMEMBER: This method **should** update cart
-end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  grand_total = 0
+  counter = 0 
+
+  ccart = consolidate_cart(cart)
+  apply_coupons(ccart, coupons)
+  apply_clearance(ccart)
+
+  while counter < ccart.length do
+    grand_total += ccart[counter][:price]*ccart[counter][:count]
+    counter += 1
+  end 
+
+   grand_total >= 100 ?  grand_total *= 0.9 : grand_total
 end
